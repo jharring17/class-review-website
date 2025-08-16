3ae814a5a47c5b6cf32e45e7540ca6f490cfd348
Jack <jackharrington1117@gmail.com>
2025-08-16 00:06:44 -0400

    updated home

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = Router();
 
 // Render the home page
 router.get('/', (req, res) => {
-	res.status(200).render('home');
+	res.status(200).render('courses/search', { title: 'Home' });
 });
 
 // Render the register page
